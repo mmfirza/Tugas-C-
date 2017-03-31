fa0d2ba464ab0eac5c1a44d6ca5a3af514347e03
Mfirza <mfirza11@gmail.com>
2017-03-31 20:31:13 +0700

    Add files via upload

---
+++ latihan2_pertemuan4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int nilai (int a, int b){
+  if ( nilai a > b ){
+	  return a;
+  }
+  return b;
+}
+
+int main (){
+	valid = nilai (15,10);
+	if b > a;
+		cout << "Nilai maksimalnya adalah : " << maksimal (10,15);
+	return b ;
+
+}
